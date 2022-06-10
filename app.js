0ab0b13b4550f09e7e5282521209255f02a6615b
Mark Schumacher <schuma1022@gmail.com>
2022-06-10 15:59:05 -0500

    Complete basic etch-a-sketch

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 const gridContainer = document.querySelector('.gridContainer');
+let div;
+let target;
 
-const gridReset = document.querySelector('.gridReset');
-gridReset.addEventListener('click', () => {
-    let gridSize = prompt("Enter grid size", "example, enter 9 for a 9x9 grid");
-    let gridSquared = gridSize*gridSize;}
-);
+function makeDivs (numOfDivs) {
+    for (let divs = 0; divs < numOfDivs; divs++) {
+        div = document.createElement('div');
+        div.classList.add('gridItem');
+        gridContainer.appendChild(div);
+    }
+};
 
-for (i = 0; i < gridSquared; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('gridItem');
-    gridContainer.appendChild(gridItem);
+function makeGrid (numOfColumns) {
+    gridContainer.style.setProperty('--numOfColumns', numOfColumns);
+    for (let columns = 0; columns < numOfColumns; columns++) {
+        makeDivs(numOfColumns);
+    }
+};
+
+function promptGridSize() {
+    let number = prompt("Choose a grid size between 5 - 64!", 16);
+        if (number >=5 && number <= 64) {
+            makeGrid (number);
+        } else {
+            do {
+                number = prompt("Invalid size, choose a number between 5 and 64!");
+            }
+            while(number < 5 || number > 64);
+            makeGrid (number)
+        }
+};
+
+function changeColor(target) {
+    target.style.backgroundColor = 'black';
 }
 
-const gridItem = document.createElement('div');
-gridItem.classList.add('gridItem');
+gridContainer.addEventListener("mouseover", e => {
+    target = e.target;
 
-//gridContainer.appendChild(gridItem);
-//comment for test
+    if (target.matches("div.gridItem")) {
+        changeColor(target);
+    }
+});
+
+const reset = document.querySelector('.gridReset');
+reset.addEventListener('click', () => {
+    window.location.reload();
+});
+
+
+promptGridSize();
